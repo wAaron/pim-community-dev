d9637c930d76821e33c4fe2adc1364810c118bea
Nicolas Dupont <nicolas@akeneo.com>
2015-04-17 09:30:09 +0200

    PIM-3832: apply master updates

--- src/Pim/Bundle/BaseConnectorBundle/Processor/Denormalization/ArrayConverter/Structured/AttributeOptionToStandardConverter.php
+++ src/Pim/Bundle/BaseConnectorBundle/Processor/Denormalization/ArrayConverter/Structured/AttributeOptionToStandardConverter.php
@@ -2,10 +2,11 @@
 
 namespace Pim\Bundle\BaseConnectorBundle\Processor\Denormalization\ArrayConverter\Structured;
 
+use Pim\Bundle\BaseConnectorBundle\Exception\ArrayConversionException;
 use Pim\Bundle\BaseConnectorBundle\Processor\Denormalization\ArrayConverter\StandardArrayConverterInterface;
 
 /**
- * Attribute Option Structured Converter
+ * Convert structured format to standard format for attribute option
  *
  * @author    Nicolas Dupont <nicola@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
@@ -42,12 +43,52 @@ class AttributeOptionToStandardConverter implements StandardArrayConverterInterf
      *     }
      * }
      */
-    public function convert(array $item, array $options = [])
+    public function convert(array $item)
     {
-        // TODO: option resolver!
+        $this->validate($item);
         $item['sort_order'] = $item['sortOrder'];
         unset($item['sortOrder']);
 
         return $item;
+    }
+
+    /**
+     * @param array $item
+     *
+     * @throws ArrayConversionException
+     */
+    protected function validate(array $item)
+    {
+        $requiredFields = ['attribute', 'code'];
+        foreach ($requiredFields as $requiredField) {
+            if (!in_array($requiredField, array_keys($item))) {
+                throw new ArrayConversionException(
+                    sprintf(
+                        'Field "%s" is expected, provided fields are "%s"',
+                        $requiredField,
+                        implode(', ', array_keys($item))
+                    )
+                );
+            }
+        }
+
+        $authorizedFields = array_merge($requiredFields, ['sortOrder', 'labels']);
+        foreach ($item as $field => $data) {
+            if (!in_array($field, $authorizedFields)) {
+                throw new ArrayConversionException(
+                    sprintf(
+                        'Field "%s" is provided, authorized fields are: "%s"',
+                        $field,
+                        implode(', ', $authorizedFields)
+                    )
+                );
+            }
+        }
+
+        if (isset($item['labels']) && !is_array($item['labels'])) {
+            throw new ArrayConversionException(
+                sprintf('Field "labels" must be an array, data provided is "%s"', print_r($item['labels'], true))
+            );
+        }
     }
 }
